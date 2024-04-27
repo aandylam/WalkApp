33f21f7e427ce957a6b070558058f31cc1bb5b0c
Andy Lam <aandylam21@gmail.com>
2024-04-27 14:27:01 -0700

    Initial Commit

---
+++ WalkApp/WalkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkAppApp.swift
+//  WalkApp
+//
+//  Created by Andy Lam on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WalkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
